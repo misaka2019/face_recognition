ffd3358c821b72e6ba472180c1b7c4fbde3ab6d3
1635573329@qq.com <misaka2019>
2021-04-22 21:43:00 +0800

    更新代码

--- setting.py
+++ setting.py
@@ -1,5 +1,5 @@
-db_info = {"host": "10.21.23.210", "user": "archive-system", "password": "archive-system-backend", "port": 3306,
-           "database": "archive-system", "charset": "utf8"}
+db_info = {"host": "127.0.0.1", "user": "root", "password": "123456", "port": 3306,
+           "database": "face", "charset": "utf8"}
 
 # 人脸识别
 milvus_param = {'collection_name': 'face_info', 'dimension': 512,
@@ -14,15 +14,11 @@ class setting(object):
         # 人脸识别
         self.db_info = db_info
         self.milvus_face_param = milvus_param
-        self.milvus_host = "10.21.23.241"
+        self.milvus_host = "127.0.0.1"
         self.milvus_port = "19530"
         self.milvus_face_collection = "face_info"
 
-        self.face_detect_net_type = "RFB"
         self.device = 'cpu'
-        self.face_detect_candidate_size = 1500
-        self.face_detect_input_size = 640
-        self.face_detect_threshold = 0.8
 
         self.face_size = 160
         self.face_recognition_pretrained = "vggface2"
